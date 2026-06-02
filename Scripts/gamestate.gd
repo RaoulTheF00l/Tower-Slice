e0050778a62c3f12ee59c7472cbb86846858ab67
RaoulTheF00l <conore21@gmail.com>
2026-06-02 16:09:54 -0400

    Added var and function stubs to gamestate.gd + created blank scripts for each scene

--- Scripts/gamestate.gd
+++ Scripts/gamestate.gd
@@ -1,1 +1,24 @@
 extends Node
+
+
+var fangs:= 0
+var wood:= 0
+var rope:= 0
+
+
+var fought_floor: bool = false
+var foraged_floor: bool = false
+
+var has_crafted_dagger: bool = false
+
+
+func reset_run() -> void:
+	pass
+
+
+func can_craft_dagger() -> void:
+	pass
+
+
+func craft_dagger() -> void:
+	pass
