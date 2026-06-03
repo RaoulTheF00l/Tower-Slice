195bc3625f86906bdd07679b4b1b2f9851cee030
RaoulTheF00l <conore21@gmail.com>
2026-06-03 13:30:13 -0400

    Added Crafting script and quit functionality on Hub scene

--- Scripts/hub.gd
+++ Scripts/hub.gd
@@ -5,39 +5,37 @@ extends Control
 @onready var craft_label: Label = $HBoxContainer/craftLabel
 @onready var quit_label: Label = $HBoxContainer/quitLabel
 
-
 var labels: Array[Label] = []
 var options := ["Explore", "Rest", "Craft", "Quit"]
 var selected_index := 0
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	labels = [
 		explore_label,
 		rest_label,
 		craft_label,
-		quit_label
+		quit_label,
 	]
-	
+
 	_update_menu()
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_right"):
 		selected_index += 1
-	if selected_index >= options.size():
-		selected_index = 0
-	
-	_update_menu()
-	
-	
+		if selected_index >= options.size():
+			selected_index = 0
+		_update_menu()
+
 	if event.is_action_pressed("ui_left"):
 		selected_index -= 1
-	if selected_index < 0:
-		selected_index = options.size() - 1
-	_update_menu()
+		if selected_index < 0:
+			selected_index = options.size() - 1
+		_update_menu()
+
+	if event.is_action_pressed("ui_accept"):
+		_select_option()
 
 
 func _update_menu() -> void:
@@ -49,4 +47,19 @@ func _update_menu() -> void:
 
 
 func _select_option() -> void:
-	pass
+	var selected_option  = options[selected_index]
+
+	if selected_option == "Explore":
+		get_tree().change_scene_to_file("res://Scene/first_floor.tscn")
+
+	elif selected_option == "Rest":
+		print("You rested. Nothing happens yet.")
+
+	elif selected_option == "Craft":
+		if Gamestate.craft_dagger():
+			print("You crafted the D-Rank Dagger! Prototype complete!")
+		else:
+			print("You need 1 Fang, 1 Silk, and 2 Wood.")
+
+	elif selected_option == "Quit":
+		get_tree().quit()
