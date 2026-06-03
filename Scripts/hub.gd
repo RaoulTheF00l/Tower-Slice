f6702756600cb03ca6ce66901663df6eaeb10dd3
RaoulTheF00l <conore21@gmail.com>
2026-06-03 12:16:00 -0400

    Checkpoint before starting hub

--- Scripts/hub.gd
+++ Scripts/hub.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var explore_label: Label = $HBoxContainer/exploreLabel
+@onready var rest_label: Label = $HBoxContainer/restLabel
+@onready var craft_label: Label = $HBoxContainer/craftLabel
+@onready var quit_label: Label = $HBoxContainer/quitLabel
+
+
+var labels: Array[Label] = []
+var options := ["Explore", "Rest", "Craft", "Quit"]
+var selected_index := 0
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
