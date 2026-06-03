5ad40885914fe5ccb266aa8d6d9ef03438c9111b
RaoulTheF00l <conore21@gmail.com>
2026-06-03 10:00:07 -0400

    Fixed Vibe coding mistake (rewrote gamestate.gd)

--- Scripts/gamestate.gd
+++ Scripts/gamestate.gd
@@ -1,36 +1,33 @@
 extends Node
 
+var fang := 0
+var silk := 0
+var wood := 0
 
-var fangs:= 0
-var wood:= 0
-var rope:= 0
-
-
-var fought_floor: bool = false
-var foraged_floor: bool = false
-
+var fought_floor_1 := false
+var foraged_floor_1 := false
+var dagger_crafted := false
 
 
 func reset_run() -> void:
-	fangs = 0
-	wood - 0
-	rope = 0
-	
-	
-	fought_floor = false
-	foraged_floor = false
+	fang = 0
+	silk = 0
+	wood = 0
+	fought_floor_1 = false
+	foraged_floor_1 = false
+	dagger_crafted = false
 
 
-func can_craft_dagger() -> void:
-	if fangs >= 2 and wood >= 2 and rope >= 3:
-		craft_dagger()
-	else: print("Not Enough materials0")
+func can_craft_dagger() -> bool:
+	return fang >= 1 and silk >= 1 and wood >= 2 and not dagger_crafted
 
 
-func craft_dagger() -> void:
-		fangs - 2
-		wood - 1
-		rope - 3
-		
-		print("You have crafted a D-Rank Dagger! End of prototype!")
-		
+func craft_dagger() -> bool:
+	if not can_craft_dagger():
+		return false
+
+	fang -= 1
+	silk -= 1
+	wood -= 2
+	dagger_crafted = true
+	return true
